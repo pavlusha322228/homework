e6b2bae100d19d69ba5c7b7375f3dc35bdad1329
pavlusha322228 <61159897+pavlusha322228@users.noreply.github.com>
2020-04-08 02:28:31 +0300

    third task

---
+++ task3/src/by/chernushov/Main.java
@@ -0,0 +1,17 @@
+package by.chernushov;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 7;
+        if (n % 10 == 7) {
+            System.out.println("true");
+        } else {
+            System.out.println("False");
+        }
+
+    }
+
+}
