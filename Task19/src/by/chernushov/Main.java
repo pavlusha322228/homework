be6a7ff2929a3ea8c3bb7c58e8ec98dda49bcc43
pavlusha322228 <61159897+pavlusha322228@users.noreply.github.com>
2020-04-08 03:18:00 +0300

    Task19 ( not without help =))

---
+++ Task19/src/by/chernushov/Main.java
@@ -0,0 +1,24 @@
+package by.chernushov;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String str;
+        int count = 0;
+        str = JOptionPane.showInputDialog("Введите текст :");
+        str = str.replaceAll("[!.,?:;-<>]", "");
+        str = str.replaceAll("-?[0-9]+", "");
+        String trim = str.trim();
+
+        String[] txt = trim.split(" +");
+        for (String word: txt){
+            count++;
+        }
+        JOptionPane.showMessageDialog(null, "Количество слов в данном тексте = " + count);
+
+    }
+}
