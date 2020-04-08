77fa1c6ad378f8cc6b7d97a52a992fe07f802c9c
pavlusha322228 <61159897+pavlusha322228@users.noreply.github.com>
2020-04-08 03:15:59 +0300

    Task13

---
+++ Task13/src/by/chernushov/Main.java
@@ -0,0 +1,34 @@
+package by.chernushov;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Введите число для проверки : ");
+            Scanner scanner = new Scanner(System.in);
+            int a = scanner.nextInt();
+            if (a == 0) {
+                System.out.println("Не вводи ноль !!!");
+                continue;
+            } else if (a < 0) {
+                System.out.println("Введите положительно число");
+                continue;
+            }
+            boolean sost = false;
+            for (int i = 2; i < a; i++) {
+                if (a % i == 0) {
+                    sost = true;
+                    break;
+                }
+            }
+            if (sost == true) {
+                System.out.println("Введенное число - составное");
+            } else {
+                System.out.println("Введенное число - простое");
+            }
+        }
+    }
+
+}
