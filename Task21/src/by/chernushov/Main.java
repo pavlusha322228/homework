368a6cfbd9984f9b5671b8fdbd01aa1f2c34c52f
pavlusha322228 <61159897+pavlusha322228@users.noreply.github.com>
2020-04-08 03:19:07 +0300

    Task21 (i hope i need to say thx for my friend)

---
+++ Task21/src/by/chernushov/Main.java
@@ -0,0 +1,33 @@
+package by.chernushov;
+
+public class Main {
+
+    static double t, t1 = 0, t2 = 0;
+    static String str = "1 java forever ";
+    static int n = 2;
+
+    public static void main(String[] args) {
+
+        StringBuilder stringBuilder = new StringBuilder(str);
+
+        for (int i = 0; i < 99; i++) {
+            t = System.currentTimeMillis();
+            str += n + " java forever ";
+            n++;
+            t1 += System.currentTimeMillis() - t;
+        }
+
+        int n = 2;
+        for (int i = 0; i < 99; i++) {
+            t = System.currentTimeMillis();
+            stringBuilder.append(n + " java forever ");
+            n++;
+            t2 += System.currentTimeMillis() - t;
+        }
+        System.out.println(str);
+        System.out.println("String execution speed: " + t1);
+        System.out.println(stringBuilder);
+        System.out.println("StringBuilder execution speed: " + t2);
+    }
+
+}
